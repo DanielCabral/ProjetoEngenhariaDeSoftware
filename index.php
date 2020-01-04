66513fac4b912fbddbc2778b29f3cb42a1d6f8cb
DanielCabral <Daniel-cabral14@hotmail.com>
2020-01-04 00:08:22 -0300

    Imagem de Fundo no login

--- index.php
+++ index.php
@@ -9,15 +9,30 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <style>
+        body { 
+            background: url(http://lorempixel.com/1920/1920/city/9/) no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .spacer {
+            margin-top: 140px; /* define margin as you see fit */
+        }
+    </style>
+
 </head>
 <body>
 <div class="container">
+    <div class="row spacer">
+        <div class="span4"></div>
+        <div class="span4"></div>
+        <div class="span4"></div>
+    </div>
     <div class="row">
     <br><br><br>
-    <aside class="col-sm-12">
-        <div class="mx-auto" style="width: 200px;heigth:100px;">
-        </div>
-    </aside>
+ 
     <aside class="col-sm-4">
     </aside>
         <aside class="col-sm-4">
